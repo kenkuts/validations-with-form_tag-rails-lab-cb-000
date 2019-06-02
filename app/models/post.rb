32a28209e050922325288cecfc519f6d6ec0ae31
kenkuts <kennethchoi19@gmail.com>
2019-06-01 21:35:30 -0400

    the real

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true # title is not blank
+  validates :content, length: { minimum: 100 }# content is at least 100 characters long
+  validates :category, inclusion: { in: %w( Fiction Non-Fiction )}# category is either "Fiction" or "Non-Fiction"
+
 end
